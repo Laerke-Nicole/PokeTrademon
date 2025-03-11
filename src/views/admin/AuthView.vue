c1f88e86ef0812e185360f11bb0eea5a05a70269
Lærke Nielsen <laenie01@easv365.dk>
2025-03-11 12:32:16 +0100

    added tailwind

--- src/views/admin/AuthView.vue
+++ src/views/admin/AuthView.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
+        <p>Log in</p>
+        <input type="text" class="" placeholder="Email"  v-model="email"/> 
+        <input type="password" class="" placeholder="Password"  v-model="password"/>
+        <button class="btn-1" @click="fetchToken(email, password)">Login</button>
 
+        <button class="" @click="logout()">Logout</button>
     </div>
 </template>
 
