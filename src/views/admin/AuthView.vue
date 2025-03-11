33479d78f0638e7ecc2d8fc019927654239a438f
Lærke Nielsen <laenie01@easv365.dk>
2025-03-11 17:23:14 +0100

    added user model and interface

--- src/views/admin/AuthView.vue
+++ src/views/admin/AuthView.vue
@@ -1,15 +1,20 @@
 <template>
-    <div>
-        <p>Log in</p>
-        <input type="text" class="" placeholder="Email"  v-model="email"/> 
-        <input type="password" class="" placeholder="Password"  v-model="password"/>
+    <div class="flex flex-col gap-4">
+        <h1>Login to your account</h1>
+        <input type="text" class="flex light-bg p-2 round-corner dark-text" placeholder="Email"  v-model="email"/> 
+        <input type="password" class="flex light-bg p-2 round-corner dark-text" placeholder="Password"  v-model="password"/>
         <button class="btn-1" @click="fetchToken(email, password)">Login</button>
-
-        <button class="" @click="logout()">Logout</button>
     </div>
 </template>
 
 <script setup lang="ts">
+// import user model
+import { useUsers } from '../../modules/auth/userModels';
+
+// get the functions needed for the login page
+const { fetchToken, email, password } = useUsers();
+
+
 
 </script>
 
