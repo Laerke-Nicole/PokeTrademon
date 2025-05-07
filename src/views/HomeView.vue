f3a18b9a7927d2a6898eb49396032ea9f25d3c1c
Lærke Nielsen <laenie01@easv365.dk>
2025-05-07 11:25:44 +0200

    fixed errors on pikachu model

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -181,6 +181,15 @@ const images = ref(Array.from({ length: 6 }, (_, i) => `https://picsum.photos/50
   align-items: center;
 }
 
+.model-container {
+  width: 100%;
+  height: 400px;
+  min-height: 400px;
+  border: 2px solid red;
+}
+
+
+
 // swiper
 .swiper {
   width: 100%;
