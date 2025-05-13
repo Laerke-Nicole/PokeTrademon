9041abc6800949063c5177b381e3c008a0f9d7a6
Lærke Nielsen <laenie01@easv365.dk>
2025-05-13 15:34:51 +0200

    redirect to auth page if user tries to access admin page and isnt logged in as well as if statement to only show the admin content if the user is logged in

--- src/views/admin/AdminView.vue
+++ src/views/admin/AdminView.vue
@@ -1,11 +1,31 @@
 <template>
-    <div>
+    <div v-if="state.userRole === 'admin'" class="five-percent dark-bg pt-18">
 
+    </div>
+
+    <!-- make sure if user isnt admin they cant see the page -->
+    <div v-else class="five-percent dark-bg pt-18">
+      <h1>Access Denied</h1>
+      <p>You do not have permission to access this page.</p>
+      <button> 
+        <RouterLink to="/">Go to Home</RouterLink>
+      </button>
+      
     </div>
 </template>
 
 <script setup lang="ts">
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
 import { scrollToTop } from '../../modules/scrollToTop/TopRouterView';
+import { state } from '../../modules/globalStates/state';
+
+const router = useRouter();
+
+// redirect to homepage if user is not admin
+if (state.userRole !== 'admin') {
+  router.push('/auth');
+}
 
 // start at the top of the page
 onMounted(() => {
