52a357bec3024756bb2f8bab9565e09b17351a22
Lærke Nielsen <laenie01@easv365.dk>
2025-05-15 17:14:46 +0200

    divided code into components for the admin add news, update news and delete news

---
+++ src/components/admin/AddNews.vue
@@ -0,0 +1,80 @@
+<template>
+    <!-- add news -->
+    <div>
+        <h2>Add News</h2>
+        <form @submit.prevent="addNewsHandler">                                                              
+        <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
+            <input type="text" v-model="newNews.title" placeholder="Title" />
+            <input type="text" v-model="newNews.subTitle" placeholder="Sub title" /> 
+
+            <div>
+            <span>News text: </span>
+            <input type="text" v-model="newNews.text" placeholder="Text" />
+            </div>
+
+            <div>
+            <span>News image: </span>
+            <input type="text" v-model="newNews.imageURL" placeholder="Image URL" />
+            </div>
+
+            <div>
+            <span>News date: </span>
+            <input type="text" v-model="newNews.date" placeholder="Date" />
+            </div>
+
+            <div>
+            <span>News theme: </span>
+            <input type="text" v-model="newNews.theme" placeholder="Theme" />
+            </div>
+
+            <div>
+            <input type="checkbox" v-model="newNews.isHidden" /> <span class="uppercase font-bold">Hidden News</span> 
+            </div>
+
+        </div>
+        <button type="submit" class="btn-1">Create news</button>
+        </form>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { useNews } from '../../modules/useNews';
+
+// news fetching
+const { fetchNews, addNews, getTokenAndUserId } = useNews();
+
+onMounted(() => {
+  fetchNews();
+});
+
+
+// create new news
+const newNews = ref({
+  title: '',
+  subTitle: '',
+  text: '',
+  imageURL: '',
+  date: '',
+  theme: '',
+  isHidden: false,
+  userId: '',
+});
+
+
+// handling adding new news
+const addNewsHandler = async () => {
+  // import userId from the useNews
+  const { userId } = getTokenAndUserId();
+  // attach to new news
+  newNews.value.userId = userId;
+  await addNews(newNews.value)
+  newNews.value = {
+    ...newNews.value,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
