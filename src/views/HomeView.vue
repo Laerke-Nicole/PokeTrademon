87423c6a7dfb018b778b514cc08c947bb16bc7b5
Lærke Nielsen <laenie01@easv365.dk>
2025-05-16 11:52:02 +0200

    added explore market into component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,25 +6,6 @@
   <NewsSection />
 
 
-  <!-- explore market -->
-  <section>
-    <div class="explore-container light-bg grid grid-cols-2 five-percent gap-12 py-18">
-      <!-- Image side -->
-      <div class="flex items-center justify-center" v-motion-fade-slide>
-        <img src="/images/pikachu-img.svg" alt="Pikachu Image" class="max-w-full h-auto" />
-      </div>
-
-      <!-- Text side -->
-      <div class="flex flex-col justify-center space-y-4 px-8">
-        <p class="yellow-text" v-motion-fade-slide>Collect cards today</p>
-        <h2 class="dark-headline pb-6" v-motion-fade-slide>More than 3000 cards to trade</h2>
-        <p class="dark-text pb-10" v-motion-fade-slide>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet perferendis voluptatum ab! Sed odit quia doloremque sapiente dicta maxime, accusantium repellat, ullam dolorem mollitia explicabo, dolorum a molestiae laboriosam laudantium.</p>
-        <RouterLink to="/profile"><button class="btn-1" v-motion-fade-slide>Start trading</button></RouterLink>
-      </div>
-    </div>
-  </section>
-
-
   <!-- about poketrademon -->
   <section>
     <div class="light-bg grid grid-cols-2 five-percent pt-18 about-poketrademon">
