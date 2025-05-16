5da83d776aee07318d46a24bb0dbbc06b4a12dc3
Lærke Nielsen <laenie01@easv365.dk>
2025-05-16 11:55:00 +0200

    added about poketrademon into component

--- src/components/homeView/AboutPoketrademon.vue
+++ src/components/homeView/AboutPoketrademon.vue
@@ -1,7 +1,22 @@
 <template>
-    <div>
-
-    </div>
+    <section>
+        <div class="light-bg grid grid-cols-2 five-percent pt-18 about-poketrademon">
+            <div>
+                <h3 class="pb-6 text-center" v-motion-fade-slide>Trade Pokémon cards!</h3>
+                <div v-motion-fade-slide>
+                    <p class="text-center text-lg">Find your dream cards, trade, and battle with fellow Trainers!</p>
+                    <p class="text-center text-lg">Poketrademon made trading quick and easy.</p>
+                </div>
+                
+                <div class="flex justify-center align-center pt-10" v-motion-fade-slide>
+                    <RouterLink to="/about"><button class="btn-1">Learn more</button></RouterLink>
+                </div>
+            </div>
+            <div v-motion-fade-slide>
+                <img src="../../../public/images/pikachu-card.png" alt="">
+            </div>
+        </div>
+    </section>
 </template>
 
 <script setup lang="ts">
@@ -9,5 +24,7 @@
 </script>
 
 <style scoped>
-
+.about-poketrademon h3 {
+  font-size: 30px;
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,25 +6,7 @@
   <NewsSection />
 
 
-  <!-- about poketrademon -->
-  <section>
-    <div class="light-bg grid grid-cols-2 five-percent pt-18 about-poketrademon">
-      <div>
-        <h3 class="pb-6 text-center" v-motion-fade-slide>Trade Pokémon cards!</h3>
-        <div v-motion-fade-slide>
-          <p class="text-center text-lg">Find your dream cards, trade, and battle with fellow Trainers!</p>
-          <p class="text-center text-lg">Poketrademon made trading quick and easy.</p>
-        </div>
-        
-        <div class="flex justify-center align-center pt-10" v-motion-fade-slide>
-          <RouterLink to="/about"><button class="btn-1">Learn more</button></RouterLink>
-        </div>
-      </div>
-      <div v-motion-fade-slide>
-        <img src="../../public/images/pikachu-card.png" alt="">
-      </div>
-    </div>
-  </section>
+  
 
 
   <!-- news -->
@@ -121,12 +103,6 @@ onMounted(() => {
 </script>
 
 <style lang="scss" scoped>
-// about
-.about-poketrademon h3 {
-  font-size: 30px;
-}
-
-
 // news
 .row-news {
   display: flex;
