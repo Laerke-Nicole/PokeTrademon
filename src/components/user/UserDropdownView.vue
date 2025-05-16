3a7d697622cb84ce20f29d789c9edd8ff42ce481
Lærke Nielsen <laenie01@easv365.dk>
2025-05-16 17:44:52 +0200

    added so you can see your username in the dropdown menu

--- src/components/user/UserDropdownView.vue
+++ src/components/user/UserDropdownView.vue
@@ -13,13 +13,15 @@
           </div>
         </RouterLink>
   
-        <div v-if="state.isLoggedIn && user">
-          <li v-if="user.username" class="w-full px-4 py-2 underline cursor-pointer">Welcome back {{ user.username }}!</li>
+        <div v-if="state.isLoggedIn">
+          <li v-if="user" class="w-full px-4 py-2">Welcome back <span class="user-name">{{ user.username }}!</span></li>
           <li v-else class="w-full px-4 py-2 italic text-gray-500">Loading user info...</li>
+          <hr class="user-hr">
         </div>
+
         <RouterLink to="/profile/edit">
-  <li class="w-full px-4 py-2 hover:bg-gray-100 cursor-pointer">Edit Profile</li>
-</RouterLink>
+          <li class="w-full px-4 py-2 hover:bg-gray-100 cursor-pointer">Edit Profile</li>
+        </RouterLink>
 
 
         <RouterLink to="/collection">
@@ -44,11 +46,16 @@
   
 
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
 import { useUsers } from '../../modules/auth/userModels';
 import { state } from '../../modules/globalStates/state';
 
 // fetch needed stuff from users
-const { user, logout } = useUsers();
+const { user, logout, loadUser } = useUsers();
+
+onMounted(async () => {
+  await loadUser(); 
+})
 </script>
 
 <style scoped>
@@ -59,6 +66,15 @@ const { user, logout } = useUsers();
 
 .dropdown ul {
   width: 100%;
+}
+
+.user-name {
+  font-weight: bold;
+}
+
+.user-hr {
+  border: 1px solid #d7d7d7;
+  margin: 0.5rem 10px;
 }
 </style>
   
