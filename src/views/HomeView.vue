fefafc0514cf6c92c31100cc2ce3fb2739ff6550
Lærke Nielsen <laenie01@easv365.dk>
2025-05-16 11:57:44 +0200

    cleared home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,14 +7,8 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, computed } from 'vue';
+import { onMounted } from 'vue';
 import { scrollToTop } from '../modules/scrollToTop/TopRouterView';
-// import swiper carousel
-import { watch } from 'vue';
-import { nextTick } from 'vue';
-import { useSwiper } from '../components/swiperCarousel/SwiperCarousel';
-import { useCards } from '../modules/useCards'
-import { useNews } from '../modules/useNews'
 
 // import components
 import HeroSection from '../components/homeView/HeroSection.vue'
@@ -23,36 +17,6 @@ import ExploreMarket from '../components/homeView/ExploreMarket.vue'
 import AboutPoketrademon from '../components/homeView/AboutPoketrademon.vue'
 import NewsSection from '../components/homeView/NewsSection.vue'
 
-
-// cards fetching
-const { loading, error, cards, fetchCards } = useCards();
-
-onMounted(() => {
-  fetchCards();
-})
-
-// only display 15 random cards
-const limitedCards = computed(() => {
-  return cards.value || [];
-});
-
-
-// fetch the data from news
-const { news, fetchNews } = useNews();
-
-onMounted(() => {
-  fetchNews();
-})
-
-
-// initialize Swiper after its loaded
-watch(loading, async (isLoading) => {
-  if (!isLoading) {
-    await nextTick(); 
-    // initialize Swiper
-    useSwiper();
-  }
-});
 
 // start at the top of the page
 onMounted(() => {
